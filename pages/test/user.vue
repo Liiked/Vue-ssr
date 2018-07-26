a70a0368348602f8db9dc9ab7b30ca32ce696f4c
Liik <linyuanqianpp@126.com>
2018-07-26 10:38:46 +0800

    添加同步请求

--- pages/test/user.vue
+++ pages/test/user.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
     这是用户页面
+    {{name}}
   </div>
 </template>
 <script>
+  import axios from 'axios'
   export default {
+    asyncData() {
+      return axios.get('http://118.24.118.235:2333').then((res)=> {
+        return {name: res.data.name}
+      })
+    },
     data() {
       return {
 
